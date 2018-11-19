d81610e86040336127ca04b07b9850f125fa63b0
Sparksong <justin.frederiksen@gmail.com>
2018-11-18 20:58:37 -0600

    Initial commit for RoboFriends App.

---
+++ src/robots.js
@@ -0,0 +1,65 @@
+//Unused now that JSON placeholder is called directly.
+
+export const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Lonk',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'WAH',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Dudez',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'StarlordMan',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Peter',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'SirBenningtonTheThird',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'AllGutsNoGlory',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Popcorn',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Clementina DuBuque',
+      username: 'MyHandIsADolphin',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Michael Scarn',
+      username: 'MichaelScarn',
+      email: 'Rey.Padberg@karina.biz'
+    }
+  ];
+  
